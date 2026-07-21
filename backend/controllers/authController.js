468d02656159f2e5345d3ae2174699bfcbe1bbcc
Viraj Kamble <virajkamble632@gmail.com>
2026-07-21 19:40:35 +0530

    Fixed production authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -87,8 +87,8 @@ export const loginUser = async(req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
