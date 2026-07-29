5e60e808d648f5b4f98253f2e8c129dd0b7d8f91
Viraj Kamble <virajkamble632@gmail.com>
2026-07-29 10:50:03 +0530

    Fix the issue of the sendEmail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,9 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
